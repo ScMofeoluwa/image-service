9f68c64c014d51e32dc0b5cb20dbffc48248f605
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-10-29 16:25:40 +0100

    fix: fixed adapter layer issues

--- app/resource/user_resource.py
+++ app/resource/user_resource.py
@@ -1,12 +1,20 @@
-from ..adapter.user_adapter import UserAdapter
+from flask import request
 from flask_restful import Resource
+from marshmallow import ValidationError
+from ..usecase.user_usecase import UserUsecase
+from ..interface.user_interface import UserSchema
+user_schema = UserSchema()
 
 
 class UserRegister(Resource):
-    def __init__(self, adapter=UserAdapter):
-        self.adapter = adapter
-
     def post(self):
-        self.adapter.add_user()
+        try:
+            user = user_schema.load(request.get_json())
+            print('load')
+        except ValidationError as err:
+            return err.messages, 400
+
+        user.password = user.generate_hash(user.password)
+        UserUsecase.register(user)
 
         return {"message": "user created successfully"}, 201
