4e7a7584929a26b6e8c7609401ae8635864b04ef
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-18 20:27:52 +0100

    feat: api key check

---
+++ app/utils.py
@@ -0,0 +1,14 @@
+from flask import request
+from functools import wraps
+from .entity.user_entity import UserModel
+
+
+def api_key_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get('x-api-key')
+        exists = UserModel.query.filter_by(api_key=api_key).first()
+        if not exists:
+            return {"description": "Request does not contain an api key.", "error": "authorization_required"}, 401
+        return f(*args, **kwargs)
+    return wrapper
