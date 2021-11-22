646fd10311a2c3a03741c27c677f346b67ac085b
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-22 13:22:39 +0100

    feat: request amount for images

--- app/resource/woof_resource.py
+++ app/resource/woof_resource.py
@@ -6,9 +6,12 @@ from flask_restful import Resource
 
 class Woof(Resource):
     @api_key_required
-    @cache.cached(timeout=30, query_string=True)
-    def get(self):
+    @cache.cached()
+    def get(self, amount):
         API_URL = "https://random.dog/woof.json"
-        r = requests.get(API_URL)
-        image_url = r.json()["url"]
-        return {"image_name": "dog", "image_url": image_url}, 200
+        res = []
+        for i in range(amount):
+            r = requests.get(API_URL)
+            image_url = r.json()["url"]
+            res.append({"image_name": "dog", "image_url": image_url})
+        return res, 200
