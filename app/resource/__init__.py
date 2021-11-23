b58c7b7e5e22375c40c8208b7c9a94165acd7460
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-23 05:33:18 +0100

    feat: picsum resource

--- app/resource/__init__.py
+++ app/resource/__init__.py
@@ -2,11 +2,13 @@ from flask_restful import Api
 from flask import Blueprint
 from ..resource.user_resource import UserRegister, UserLogin, UserLogout
 from ..resource.woof_resource import Woof
+from ..resource.picsum_resource import Picsum
 
-resource_bp = Blueprint('resource', __name__)
-api = Api(resource_bp, prefix='/api/v1')
+resource_bp = Blueprint("resource", __name__)
+api = Api(resource_bp, prefix="/api/v1")
 
-api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegister, "/register")
 api.add_resource(UserLogin, "/login")
 api.add_resource(UserLogout, "/logout")
-api.add_resource(Woof, '/woof/<int:amount>')
+api.add_resource(Woof, "/woof/<int:amount>")
+api.add_resource(Picsum, "/picsum/<int:amount>")
