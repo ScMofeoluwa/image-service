f3bb9f14f1f902cac5f218f4c973b56f6d555d30
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-25 00:23:25 +0100

    chore: small changes

--- app/utils.py
+++ app/utils.py
@@ -6,9 +6,13 @@ from .entity.user_entity import UserModel
 def api_key_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        api_key = request.headers.get('x-api-key')
+        api_key = request.headers.get("x-api-key")
         exists = UserModel.query.filter_by(api_key=api_key).first()
         if not exists:
-            return {"description": "Request does not contain an api key.", "error": "authorization_required"}, 401
+            return {
+                "description": "Request does not contain an api key.",
+                "error": "authorization_required",
+            }, 401
         return f(*args, **kwargs)
+
     return wrapper
